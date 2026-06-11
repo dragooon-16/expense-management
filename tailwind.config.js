ff272c0183d8eb4bc0647030600b9ba76524f4bd
dragooon-16 <milindgoyal16@gmail.com>
2026-06-11 20:50:21 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,10 @@ module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        darkBg: "#121212",
-        panelBg: "#1a1a1a",
-        cardBg: "#242424",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
