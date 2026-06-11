ced88f9959a67a04d65e4880afa54bec715f62a1
dragooon-16 <milindgoyal16@gmail.com>
2026-06-11 17:42:54 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOs9bfpKoFLa9QUCHB3itqY8lCJKCsRMs",
-  authDomain: "travelexp-eeddb.firebaseapp.com",
-  projectId: "travelexp-eeddb",
-  storageBucket: "travelexp-eeddb.firebasestorage.app",
-  messagingSenderId: "958565272542",
-  appId: "1:958565272542:web:46e4457925f8b35adc2b4f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
